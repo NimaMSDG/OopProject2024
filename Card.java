3ac5e9f09e09f8bf25233cbc34f176b3ba0614d9
Nima <159201535+NimaMSDG@users.noreply.github.com>
2024-06-25 18:36:41 +0330

    Add files via upload

--- Card.java
+++ Card.java
@@ -1,21 +1,63 @@
- abstract public class Card {
+import java.util.ArrayList;
+import java.util.Arrays;
 
-    int duration,damage,level,number;
+public abstract class Card implements Comparable<Card> , Cloneable{
     String name;
+    int duration;
+    int damage;
+    int price;
+    int level;
+    int id;
+    Type type;
     Character character;
+    public abstract String properties();
+    public abstract String doAbility(ArrayList<Card> map);
 
 
-    public int segmentDamage(){
-        return damage/duration;
+
+
+    public void levelUp(){
+        level++;
     }
 
 
-    Card(){
-
+    @Override
+    public Card clone() {
+        try {
+            Card clone = (Card) super.clone();
+            return clone;
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError();
+        }
+    }
+    public enum Type{
+        Attack,Defence,Spell;
     }
 
 
-    public enum Character{
-        DarthVader,Luke,BobaFett,Mandalorian;
+    //sort cards arraylist
+    @Override
+    public int compareTo(Card card) {
+        Integer n1=1,n2=1;
+        if (this.type.equals(Type.Attack)) n1=1;
+        if (this.type.equals(Type.Defence)) n1=2;
+        if (this.type.equals(Type.Spell)) n1=3;
+        if (card.type.equals(Type.Attack)) n2=1;
+        if (card.type.equals(Type.Defence)) n2=2;
+        if (card.type.equals(Type.Spell)) n2=3;
+        return n1.compareTo(n2);
+    }
+
+    public ArrayList<Card> sortCards(ArrayList<Card> cards){
+        Card[] cards1 = new Card[cards.size()];
+        for (int i=0;i<cards.size();i++){
+            cards1[i] = cards.get(i);
+        }
+        Arrays.sort(cards1);
+        ArrayList<Card> result = new ArrayList<>();
+        for (int i=0;i<cards.size();i++){
+            result.add(cards1[i]);
+        }
+        return result;
     }
 }
